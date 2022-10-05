c966ade6b925f64f4701b18363e28a37cd8ed7c4
Hector Gonzalez <gorozco.hector@gmail.com>
2022-10-05 15:49:17 -0400

    Style cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ import '../styles/cardStyle.css'
 function Card(props){
     return (
         <div className='card'>
-            <p>{props.name}</p>
             <img src={props.image} alt="" />
+            <p>{props.name}</p>
         </div>
     )
 }
--- src/components/CardsArea.jsx
+++ src/components/CardsArea.jsx
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Card'
 
+
+
 function CardsArea(props){
     const [cards, setCards] = useState([]);
     // This is used to stop an extra api call with react strict mode.
     let fetched = false;
 
+    // Returns a string with num character ids,
+    // in a format suitable to be used by the API
+    function getRandomCharactersIDs(num){
+        const ids = new Set()
+        while(ids.size < num){
+            // get a random number from 0 to 826
+            ids.add(Math.floor(Math.random() * 826))
+        }
+        return [...ids]
+    }
+
     // Fetches characters from rick and morty API
     async function getCharacters(){
+        console.log(getRandomCharactersIDs(10))
         fetched = true;
-        await fetch("https://rickandmortyapi.com/api/character", {
+        try{
+        await fetch(`https://rickandmortyapi.com/api/character/${getRandomCharactersIDs(10)}`, {
           method: "GET",
           mode: "cors",
         })
           .then((data) => data.json())
           .then((json) => {
-            [...json.results].forEach((character) => {
+            console.log(json);
+            [...json].forEach((character) => {
               const {name, image} = character;
               setCards(prevCards => [...prevCards, {name, image}]);
             }
             );
           });
+        } catch (error){
+            console.log(error)
+        }
     }
 
     // fetch data from api on mount
